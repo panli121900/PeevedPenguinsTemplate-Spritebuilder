9c62cd2494d8b79f0e487bc1bbcb7076192f26df
apple <apple@appledeMacBook-Air.local>
2015-07-10 22:32:53 +0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -7,40 +7,40 @@
 //
 
 #import "Gameplay.h"
-
-@implementation Gameplay{
-    CCPhysicsNode *_physicsNode;
-    CCNode *_catapultArm;
-}
-
-//this is automatically called when CCB completes loading
--(void)didLoadFromCCB{
-    
-    //enable touch
-    self.userInteractionEnabled = YES;
-}
-
-//automatically called when touch happens
--(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
-    
-    //launch without directions
-    [self launchPenguin];
-}
-
--(void)launchPenguin{
-    // load the penguin
-    CCNode *penguin = [CCBReader load:@"Penguin"];
-    
-    //move its position to the catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
-    
-    // give the penguin physics
-    [_physicsNode addChild:penguin];
-    
-    //apply a force to it using the built-in physics
-    CGPoint launchDirection = ccp(1,0);
-    CGPoint force = ccpMult(launchDirection, 8000);
-    [penguin.physicsBody applyForce:force];
-}
+//
+//@implementation Gameplay{
+//    CCPhysicsNode *_physicsNode;
+//    CCNode *_catapultArm;
+//}
+//
+////this is automatically called when CCB completes loading
+//-(void)didLoadFromCCB{
+//    
+//    //enable touch
+//    self.userInteractionEnabled = YES;
+//}
+//
+////automatically called when touch happens
+//-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
+//    
+//    //launch without directions
+//    [self launchPenguin];
+//}
+//
+//-(void)launchPenguin{
+//    // load the penguin
+//    CCNode *penguin = [CCBReader load:@"Penguin"];
+//    
+//    //move its position to the catapult
+//    penguin.position = ccpAdd(_catapultArm.position, ccp(16,50));
+//    
+//    // give the penguin physics
+//    [_physicsNode addChild:penguin];
+//    
+//    //apply a force to it using the built-in physics
+//    CGPoint launchDirection = ccp(1,0);
+//    CGPoint force = ccpMult(launchDirection, 8000);
+//    [penguin.physicsBody applyForce:force];
+//}
 
 @end
